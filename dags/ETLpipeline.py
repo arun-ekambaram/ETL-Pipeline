aaddc0321cdb46b0a632daf5f3bace00770d7163
arun ekambaram <arunvit1730@gmail.com>
2024-10-21 15:45:19 +0530

    created the table in Postgresql

---
+++ dags/ETLpipeline.py
@@ -0,0 +1,54 @@
+from airflow import DAG
+# SimpleHttpOperator is basically linked with the task where when we will be calling with the APIs 
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.decorators import task
+# PostgresHook will be linked with that particular task, wherein we will be pushing the data into the PostgreSQL.
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.utils.dates import days_ago
+import json
+
+#Define the DAG
+
+with DAG(
+    dag_id ='nasa_apod_postgres',
+    start_date = days_ago(1),
+    schedule_interval = '@daily',
+    catcup = False
+) as dag:
+    
+    ## step 1: Create the table if it doesn't exist
+
+    @task
+    def create_table():
+        ## initialize the PostgresHook
+        postgres_hook = PostgresHook(postgres_conn_id="my_postgres_connection")
+        ## SQL query to create the table
+        create_table_query="""
+        CREATE TABLE IF NOT EXISTS apod_data (
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(255),
+            explanation TEXT,
+            url TEXT,
+            date DATE,
+            media_type VARCHAR(50)
+            );
+        
+
+        """
+        ##Execute SQL query
+        postgres.hook.run(create_table_query)
+
+
+    ## step 2: Extract the NASA API Data(APOD) - Astronomy Picture of the day [Extract Pipleline]
+    # https://api.nasa.gov/planetary/apod?api_key=HdXTz8iHZ7HGKYiyXazXkrwKnPOHQnhNCd1Tj3r7
+   
+  
+    ## step 3: Transform the data(Pick the information that i need to save)
+
+  
+    ## step 4: Load the data into PostgresSQL
+
+
+    ## step 5: Verify the data DBViewer (will help you to connect to any database)
+
+    ## step 6: Define the task dependencies
